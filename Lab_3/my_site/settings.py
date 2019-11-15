cdd447240ae668f9b00b8749b100fb9c83920e59
Oleksii <oleksii.hashchenko.it.2017@lpnu.ua>
2019-11-15 20:15:29 +0200

    Settings.py changed vol 3

--- Lab_3/my_site/settings.py
+++ Lab_3/my_site/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'qo+r=3gn)xx1e5487@o+3y9-esy7(p@vr61j*d%i+on_5sdgf5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0',
+    '127.0.0.1'
+]
 
 
 # Application definition
